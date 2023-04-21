d4a19bb743f075a56f782325b2f9d58f13268f30
AlexxMotta <alexmota@alu.ufc.br>
2023-04-21 12:43:32 -0300

    creating DbContext

---
+++ SistemaDeTarefas/Data/TasksDBContex.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class TasksDBContex : DbContext
+    {
+        public TasksDBContex(DbContextOptions<TasksDBContex> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
