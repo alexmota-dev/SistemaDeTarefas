95bc442836d283115d5c2dbd2141e64703acf396
AlexxMotta <alexmota@alu.ufc.br>
2023-04-21 13:11:27 -0300

    creating interface user

---
+++ SistemaDeTarefas/Repositories/Interfaces/InterfaceUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositories.Interfaces
+{
+    public interface InterfaceUserRepository
+    {
+        Task<List<UserModel>> FindAllUsers();
+        Task<UserModel> FindById(int id);
+        Task<UserModel> Create(UserModel user);
+        Task<UserModel> Update(UserModel user, int id);
+        Task Delete(int id);
+    }
+}
