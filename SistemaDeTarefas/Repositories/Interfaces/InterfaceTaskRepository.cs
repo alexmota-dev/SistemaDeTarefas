6e4c586fc223ec51adef6152e88c120c50c1ea21
AlexxMotta <alexmota@alu.ufc.br>
2023-04-23 12:42:26 -0300

    Creating Interface Task Repository

---
+++ SistemaDeTarefas/Repositories/Interfaces/InterfaceTaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositories.Interfaces
+{
+    public interface InterfaceTaskRepository
+    {
+        Task<List<TaskModel>> FindAll();
+        Task<TaskModel> FindById(int id);
+        Task<TaskModel> Create(TaskModel Task);
+        Task<TaskModel> Update(TaskModel Task, int id);
+        Task<bool> Delete(int id);
+    }
+}
