9341956559c353d68e0ff3fc15cbac64ee55d79e
AlexxMotta <alexmota@alu.ufc.br>
2023-04-23 12:44:37 -0300

    creating findAll in taskController

---
+++ SistemaDeTarefas/Controllers/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositories.Interfaces;
+
+namespace SistemaDeTarefas.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly InterfaceTaskRepository _taskRepository;
+
+        public TaskController(InterfaceTaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TaskModel>>> FindAll()
+        {
+            List<TaskModel> task = await _taskRepository.FindAll();
+            return Ok(task);
+        }
+    }
+}
