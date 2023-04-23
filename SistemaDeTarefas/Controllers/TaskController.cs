036784fb47d27619db75a3defafab2ab0ad90a90
AlexxMotta <alexmota@alu.ufc.br>
2023-04-23 13:03:01 -0300

    Creating function findById, Create, Update and Delete in TaskController.

--- SistemaDeTarefas/Controllers/TaskController.cs
+++ SistemaDeTarefas/Controllers/TaskController.cs
@@ -19,5 +19,56 @@ namespace SistemaDeTarefas.Controllers
             List<TaskModel> task = await _taskRepository.FindAll();
             return Ok(task);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskModel>> FindById(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest("Id inválido");
+            }
+            TaskModel task = await _taskRepository.FindById(id);
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskModel>> Create([FromBody] TaskModel taskModel)
+        {
+            if (taskModel == null)
+            {
+                return BadRequest("Body Inválido");
+            }
+            TaskModel task = await _taskRepository.Create(taskModel);
+            return Ok(task);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TaskModel>> Update([FromBody] TaskModel taskModel, int id)
+        {
+            if(taskModel == null || id < 1)
+            {
+                return BadRequest("Requisição invalida, verifique o id e o body enviados.");
+            }
+
+            taskModel.Id = id;
+            TaskModel task = await _taskRepository.Update(taskModel, id);
+            return Ok(task);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TaskModel>> Delete(int id)
+        {
+            bool taskDeleted = await _taskRepository.Delete(id);
+            if (taskDeleted)
+            {
+                return Ok("Task Deletada");
+            }
+            else
+            {
+                return Ok("Task não existe no banco");
+            }
+        }
+
+
     }
 }
